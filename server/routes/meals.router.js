4ac964d6f873741032a1cbcd494d046782a9fa20
Chris Hess <65086478+hesscm@users.noreply.github.com>
2021-10-14 09:27:14 -0500

    added a test post query in the meals router

--- server/routes/meals.router.js
+++ server/routes/meals.router.js
@@ -8,15 +8,41 @@ const router = express.Router();
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     // GET route code here
 });
 
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
+router.post('/',  async (req, res) => {
     // POST route code here
+    try {
+        await pool.query('BEGIN');
+        const queryTest = `WITH json_array AS ( SELECT
+         4463899,
+        'recipe name',
+        'desc',
+        'inst',
+        'img.path',
+        'Monday',
+        jsonb_array_elements('[{
+			"col1": "a",
+            "col2": 1,
+            "col3": 1,
+            "col4": "one"
+                 }]'::jsonb))
+        INSERT INTO meals(id, name, description, instructions, day, image_path, ingredients) 
+        SELECT * FROM json_array;`;
+        const values = []; //FIX ME WHEN READY TO CONNECT THE WIRES FROM CLIENT
+        await pool.query(queryTest);
+        await pool.query('COMMIT');
+        res.sendStatus(201);
+    } catch(error) {
+        console.log('ROLLBACK', error);
+        await pool.query('ROLLBACK');
+        throw error;
+    }
 });
 
 module.exports = router;
