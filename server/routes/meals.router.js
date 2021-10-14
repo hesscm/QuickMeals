db6cc945eb1caab3f7cd648ae7bb59d45e15cfd6
Chris Hess <65086478+hesscm@users.noreply.github.com>
2021-10-14 15:21:26 -0500

    created a successful loop in the router that only post meals that the user has added

--- server/routes/meals.router.js
+++ server/routes/meals.router.js
@@ -18,21 +18,29 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
     // POST route code here
     console.log('id', req.user.id);
-    const meal = req.body[0];
+    const meal = req.body;
     console.log(meal);
     try {
         await pool.query('BEGIN');
-        const queryTest = `
+        for (let i = 0; i < meal.length; i++) {
+            if (meal[i].title == '') {
+                console.log('Nothing to input at element ' + i);
+            } else {
+                const queryTest = `
                 INSERT INTO meals (api_id, name, description, instructions, image_path, day, ingredients)
                 VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING "id";`;
-        const values = [meal.id, meal.title, meal.description, meal.instructions, meal.image, meal.day, JSON.stringify(meal.ingredients)]; //FIX ME WHEN READY TO CONNECT THE WIRES FROM CLIENT
-        const mealsResult = await pool.query(queryTest, values);
-        console.log(mealsResult);
-        const mealsID = mealsResult.rows[0].id;
-        console.log(mealsID);
-        const junctionQuery = `INSERT INTO user_meals (user_id, meals_id) VALUES($1, $2);`;
-        junctionValues = [req.user.id, mealsID];
-        await pool.query(junctionQuery, junctionValues);
+                const values = [meal[i].id, meal[i].title, meal[i].description, meal[i].instructions, meal[i].image, meal[i].day, JSON.stringify(meal[i].ingredients)]; //FIX ME WHEN READY TO CONNECT THE WIRES FROM CLIENT
+                const mealsResult = await pool.query(queryTest, values);
+                console.log(mealsResult);
+                const mealsID = mealsResult.rows[0].id;
+                console.log(mealsID);
+                const junctionQuery = `INSERT INTO user_meals (user_id, meals_id) VALUES($1, $2);`;
+                junctionValues = [req.user.id, mealsID];
+                await pool.query(junctionQuery, junctionValues);
+            }
+            
+        }
+
         await pool.query('COMMIT');
         res.sendStatus(201);
     } catch (error) {
